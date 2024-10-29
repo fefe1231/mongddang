3070077a1dd2eda3cbbcd9c36e454f3e77e1211a
변서원 <qustjdnjs@naver.com>
2024-10-29 15:49:10 +0900

    :sparkles: feat/(#25,S11P31D207-166) : CtoP,User model 작성

---
+++ backend/mongddang/src/main/java/com/onetwo/mongddang/domain/user/model/User.java
@@ -0,0 +1,48 @@
+package com.onetwo.mongddang.domain.user.model;
+
+import com.nimbusds.openid.connect.sdk.claims.Gender;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import org.checkerframework.checker.units.qual.N;
+
+import java.time.LocalDate;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @NotNull
+    private LocalDate birth;
+
+    private String invitationCode;
+
+    @Column(nullable = false, length = 10)
+    private String nickname;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String email;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+
+    public enum Role {
+        child, protector
+    };
+
+    public enum Gender {
+        male, female
+    };
+
+}
