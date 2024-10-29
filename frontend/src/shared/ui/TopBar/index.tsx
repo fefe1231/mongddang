9a58dd3ac98e8c4901f61261186d27ecca14767c
조선미 <tjs2194@naver.com>
2024-10-29 15:26:25 +0900

    ✨ Feat/(#24, S11P31D207-135): 코드리뷰 수정 완료

--- frontend/src/shared/ui/TopBar/index.tsx
+++ frontend/src/shared/ui/TopBar/index.tsx
@@ -4,6 +4,7 @@ import { TopBarProps } from './TopBar.types';
 import { base } from './TopBar.styles';
 import { IoIosArrowBack, IoIosClose } from 'react-icons/io';
 import { Icon } from '../Icon';
+import { Typography } from '../Typography';
 
 export const TopBar = ({
   children = '테스트',
@@ -15,7 +16,9 @@ export const TopBar = ({
     <div css={base(type)} {...props}>
       {type === 'page' && (
         <>
-          <span>{children}</span>
+          <Typography color="light" size="1.25" weight={600}>
+            {children}
+          </Typography>
         </>
       )}
       {type === 'iconpage' && (
@@ -26,12 +29,21 @@ export const TopBar = ({
               style={{ marginRight: '0.5rem' }}
             />
           </Icon>
-          <span>{children}</span>
+          <Typography color="light" size="1.25" weight={600}>
+            {children}
+          </Typography>
         </>
       )}
       {type === 'modal' && (
         <>
-          <span style={{ flexGrow: 1, textAlign: 'center' }}>{children}</span>
+          <Typography
+            style={{ flexGrow: 1, textAlign: 'center' }}
+            color="light"
+            size="1.25"
+            weight={600}
+          >
+            {children}
+          </Typography>
           <Icon size={2}>
             <IoIosClose onClick={() => iconHandler()} />
           </Icon>
