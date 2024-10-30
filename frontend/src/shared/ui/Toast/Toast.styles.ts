b24a8d4336faa9b98070f03341d0f77c3981945c
조선미 <tjs2194@naver.com>
2024-10-30 11:14:56 +0900

    ✨ Feat/(#32, S11P31D207-145): 토스트 구현

---
+++ frontend/src/shared/ui/Toast/Toast.styles.ts
@@ -0,0 +1,47 @@
+import { SerializedStyles, Theme, css } from '@emotion/react';
+
+import { ToastVariant } from './Toast.types';
+import { Palette } from '@/shared/model/globalStylesTyes';
+import ColorStyle from '../styles/colorStyles';
+
+const colors: Record<Palette, string> = {
+  primary: '#F3E5F5',
+  success: '#E3F2FD',
+  danger: '#FFEBEE',
+  secondary: '#EBEBEB',
+  indigo:'#3F51B5', 
+  blue:'#2196F3',
+  dark: '#F3E5F5',
+  light: '#F3E5F5',
+};
+
+export const base = () => css`
+  display: flex;
+  align-items: center;
+  column-gap: 1rem;
+  width: fit-content;
+  max-width: calc(100% - 2rem);
+  padding: 1rem 1rem;
+  border-radius: 0.25rem;
+  font-size: 1rem;
+  border-radius: 0.625rem;
+`;
+
+export const variants: Record<
+  ToastVariant,
+  (theme: Theme, color: Palette) => SerializedStyles
+> = {
+  contained: (theme: Theme, color: Palette) => css`
+    background-color: ${colors[color]};
+    font-size: 1rem;
+  `,
+  filled: (theme: Theme, color: Palette) => css`
+    background-color: ${ColorStyle[color].main};
+    font-size: 1rem;
+  `,
+  outlined: (theme: Theme, color: Palette) => css`
+    background-color: white;
+    border: 1px solid ${ColorStyle[color].main};
+    font-size: 1rem;
+  `,
+};
---
+++ frontend/src/shared/ui/Toast/Toast.types.ts
@@ -0,0 +1,10 @@
+import { Palette } from '@/shared/model/globalStylesTyes';
+import { ReactNode } from 'react';
+
+export type ToastVariant = 'contained' | 'filled' | 'outlined';
+
+export interface ToastProps extends React.ComponentProps<'div'> {
+  children: ReactNode;
+  variant?: ToastVariant;
+  color?: Palette;
+}
