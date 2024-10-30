7646da41ebb828c309d17c76ee8b6b5f2a496500
조선미 <tjs2194@naver.com>
2024-10-30 13:22:51 +0900

    🐛 Fix/(#35, S11P31D207-202) : Backdrop 컬러타입 추가

--- frontend/src/shared/ui/Backdrop/Backdrop.types.ts
+++ frontend/src/shared/ui/Backdrop/Backdrop.types.ts
@@ -1,7 +1,9 @@
+import { Palette } from '@/shared/model/globalStylesTyes';
 import { ReactNode } from 'react';
 
 export interface BackdropProps extends React.ComponentProps<'div'> {
   children?: ReactNode;
   opacity?: number;
   blur?: number;
+  color?: Palette;
 }
