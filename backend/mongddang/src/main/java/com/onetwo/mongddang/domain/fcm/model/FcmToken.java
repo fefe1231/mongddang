68666d66275364ad6c1066bb519290bafcfbfc28
변서원 <qustjdnjs@naver.com>
2024-10-31 03:14:55 +0900

    :sparkles: feat/(#36,S11P31D207-163) : fcm 등록기능 구현
    - 에러 코드 정리 필요

--- backend/mongddang/src/main/java/com/onetwo/mongddang/domain/fcm/model/FcmToken.java
+++ backend/mongddang/src/main/java/com/onetwo/mongddang/domain/fcm/model/FcmToken.java
@@ -2,10 +2,19 @@ package com.onetwo.mongddang.domain.fcm.model;
 
 import com.onetwo.mongddang.domain.user.model.User;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@Builder
+@NoArgsConstructor // JPA용 기본 생성자
+@AllArgsConstructor // Builder에서 모든 필드를 초기화하는 생성자
 public class FcmToken {
 
     @Id
@@ -13,8 +22,14 @@ public class FcmToken {
     private Long id;
 
     @ManyToOne
+    @NotNull
     private User user;
+
+    @NotNull
     private String token;
-    private LocalDateTime created_at;
-    private LocalDateTime updated_at;
+
+    @NotNull
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
 }
---
+++ backend/mongddang/src/main/java/com/onetwo/mongddang/domain/fcm/service/DeviceTokenService.java
@@ -0,0 +1,53 @@
+package com.onetwo.mongddang.domain.fcm.service;
+
+import com.onetwo.mongddang.domain.fcm.model.FcmToken;
+import com.onetwo.mongddang.domain.fcm.repository.FcmTokenRepository;
+import com.onetwo.mongddang.domain.user.model.User;
+import com.onetwo.mongddang.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class DeviceTokenService {
+
+    private final FcmTokenRepository fcmTokenRepository;
+    private final UserRepository userRepository;
+
+    public void saveOrUpdateToken(String token, Long userId) {
+        Optional<FcmToken> existingToken = fcmTokenRepository.findByUserId(userId);
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("User not found with id: " + userId));
+
+        if (existingToken.isPresent()) {
+            log.info("fcm token 있음");
+            FcmToken tokenRecord = existingToken.get();
+            tokenRecord = tokenRecord.builder()
+                    .user(tokenRecord.getUser())
+                    .createdAt(tokenRecord.getCreatedAt())
+                    .token(token) // 토큰 업데이트
+                    .updatedAt(LocalDateTime.now()) // 업데이트 시간 설정
+                    .build();
+            fcmTokenRepository.save(tokenRecord);
+            log.info("fcm token 업데이트 완료");
+        } else {
+            log.info("fcm token 없음");
+            FcmToken newToken = FcmToken.builder()
+                    .user(user)
+                    .token(token)
+                    .updatedAt(LocalDateTime.now())
+                    .createdAt(LocalDateTime.now())
+                    .build();
+            fcmTokenRepository.save(newToken); // 없으면 저장
+            log.info("fcm token 저장 완료");
+
+        }
+    }
+
+}
