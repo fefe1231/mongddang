fe2e80488cd3f905be5a41ba4d45afdc3873d56d
조선미 <tjs2194@naver.com>
2024-10-31 09:43:36 +0900

    ✨ Feat/(#40, S11P31D207-204): 회원가입 경로 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Signup } from './pages/Signup';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Routes>
+    <Route path="/signup" element={<Signup />} />
+    </Routes>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
---
+++ frontend/src/pages/Signup/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Signup = () => {
+  return (
+    <div>
+      회원관리 페이지
+    </div>
+  )
+}
