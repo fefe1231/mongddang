eee021d498cb4f2550cd96bc17f0347d774e850f
youngentry <gentry_@naver.com>
2024-10-31 13:22:50 +0900

    :sparkle: Feat/(#15, S11P31D207-155) : 코인 관련 서비스 컨트롤러 수정

--- backend/mongddang/src/main/java/com/onetwo/mongddang/domain/game/coinLog/controller/CoinLogController.java
+++ backend/mongddang/src/main/java/com/onetwo/mongddang/domain/game/coinLog/controller/CoinLogController.java
@@ -1,6 +1,5 @@
 package com.onetwo.mongddang.domain.game.coinLog.controller;
 
-import com.onetwo.mongddang.domain.game.coinLog.dto.RequestGetCoinDto;
 import com.onetwo.mongddang.domain.game.coinLog.dto.RequestRewardCoinDto;
 import com.onetwo.mongddang.domain.game.coinLog.dto.ResponseCoin;
 import com.onetwo.mongddang.domain.game.coinLog.model.CoinLog;
@@ -23,43 +22,17 @@ public class CoinLogController {
 
     /**
      * 보유 코인을 반환
-     * @param requestGetCoinDto
      * @return ResponseCoin
      */
     @GetMapping("/coin")
-    public ResponseEntity<ResponseCoin> getRemainCoin(@RequestBody RequestGetCoinDto requestGetCoinDto, HttpServletRequest request) {
+    public ResponseEntity<ResponseCoin> getRemainCoin(HttpServletRequest request) {
         log.info("GET /api/game/coin/");
 
         // 유저의 id를 jwt 에서 추출
         Long userId = jwtExtratService.jwtFindId(request);
 
         // 빌더 패턴을 사용하여 ResponseCoin 객체 생성
-        ResponseCoin response = ResponseCoin.builder()
-                .coin(coinLogService.getCoinCount(userId))
-                .build();
-
-        return ResponseEntity.ok(response);
-    }
-
-    /**
-     * 코인을 지급하고 지급된 코인량을 반환
-     * @param requestRewardCoinDto
-     * @return ResponseCoin
-     */
-    @PostMapping("/coin/reward")
-    public ResponseEntity<ResponseCoin> rewardCoin(@RequestBody RequestRewardCoinDto requestRewardCoinDto, HttpServletRequest request) {
-        log.info("POST /api/game/coin/reward");
-
-        // 유저의 id를 jwt 에서 추출
-        Long userId = jwtExtratService.jwtFindId(request);
-
-        // 코인 지급
-        CoinLog newCoinLog = coinLogService.rewardCoin(userId, requestRewardCoinDto.getCategory(), requestRewardCoinDto.getCoin());
-
-        // 빌더 패턴을 사용하여 ResponseCoin 객체 생성
-        ResponseCoin response = ResponseCoin.builder()
-                .coin(newCoinLog.getCoin())
-                .build();
+        ResponseCoin response = coinLogService.responseGetCoinCount(userId);
 
         return ResponseEntity.ok(response);
     }
--- backend/mongddang/src/main/java/com/onetwo/mongddang/domain/game/coinLog/service/CoinLogService.java
+++ backend/mongddang/src/main/java/com/onetwo/mongddang/domain/game/coinLog/service/CoinLogService.java
@@ -1,9 +1,8 @@
 package com.onetwo.mongddang.domain.game.coinLog.service;
 
-import com.onetwo.mongddang.domain.game.coinLog.model.CoinLog;
-import com.onetwo.mongddang.domain.game.coinLog.model.CoinLog.CoinCategory;
+import com.onetwo.mongddang.domain.game.coinLog.application.CoinLogUtils;
+import com.onetwo.mongddang.domain.game.coinLog.dto.ResponseCoin;
 import com.onetwo.mongddang.domain.game.coinLog.repository.CoinLogRepository;
-import com.onetwo.mongddang.domain.user.model.User;
 import com.onetwo.mongddang.domain.user.repository.UserRepository;
 import lombok.Builder;
 import lombok.extern.slf4j.Slf4j;
@@ -16,82 +15,21 @@ public class CoinLogService {
 
     private final CoinLogRepository coinLogRepository;
     private final UserRepository userRepository;
+    private final CoinLogUtils coinLogUtils;
 
     /**
-        * 유저의 보유 코인을 조회합니다.
+        * 보유 코인 응답 객체를 반환합니다.
         * @param id 코인을 조회할 유저의 id
-        * @return 보유 코인량
+        * @return ResponseCoin
      */
-    public int getCoinCount(Long id) {
-        // userId에 해당하는 CoinLog 조회
-        CoinLog coinLog = coinLogRepository.findTopByChildIdOrderByIdDesc(id).orElseThrow(() -> new RuntimeException("Coin log not found for userId: " + id));
+    public ResponseCoin responseGetCoinCount(Long id) {
+        log.info("responseGetCoinCount id: {}", id);
+
+        ResponseCoin response = ResponseCoin.builder()
+                .coin(coinLogUtils.getCoinCount(id))
+                .build();
 
         // 현재 coin 값 반환
-        return coinLog.getCoin();
+        return response;
     }
-
-    /**
-        * 코인을 지급합니다.
-        * @param id 코인을 지급할 유저의 id
-        * @param category 코인 지급 카테고리
-        * @param amount 지급할 코인량
-        * @return 지급된 코인 로그
-        * @throws IllegalArgumentException amount 가 0 이하인 경우
-     */
-    public CoinLog rewardCoin(Long id, CoinCategory category, int amount) {
-        // amount 가 0 이하인 경우 예외 처리
-        if (amount <= 0) {
-            throw new IllegalArgumentException("amount should be positive");
-        }
-
-        // id 에 해당하는 User 조회
-        User user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found for id: " + id));
-
-        // 새로운 CoinLog 기록 추가
-        int remainCoin = getCoinCount(id); // 보유 코인
-        CoinLog newCoinLog = CoinLog.builder()
-                .child(user) // 유저
-                .coin(remainCoin + amount) // 지급량
-                .category(category) // 카테고리
-                .build();
-
-        // 새로운 CoinLog 저장
-        coinLogRepository.save(newCoinLog);
-
-        return newCoinLog;
-    }
-
-    /**
-        * 코인을 차감합니다.
-        * @param id 유저의 id
-        * @param category 코인 차감 카테고리
-        * @param amount 차감할 코인량
-        * @return 차감된 코인 로그
-        * @throws IllegalArgumentException amount 가 0 이하인 경우
-     */
-    public CoinLog minusCoin(Long id, CoinCategory category, int amount) {
-        // id 에 해당하는 User 조회
-        User user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found for id: " + id));
-
-        // userId에 해당하는 CoinLog 조회
-        CoinLog coinLog = coinLogRepository.findTopByChildIdOrderByIdDesc(id).orElseThrow(() -> new RuntimeException("해당 유저에 대한 CoinLog 를 찾을 수 없습니다. id: " + id));
-
-        // 별가루가 부족할 경우 예외 처리
-        if (coinLog.getCoin() - amount < 0) {
-            throw new IllegalArgumentException("별가루가 부족합니다.");
-        }
-
-        // 새로운 CoinLog 기록 추가
-        CoinLog newCoinLog = CoinLog.builder()
-                .child(user) // 유저
-                .coin(coinLog.getCoin() - amount) // 차감량
-                .category(CoinCategory.mongddang) // 카테고리
-                .build();
-
-        // 새로운 CoinLog 저장
-        coinLogRepository.save(newCoinLog);
-
-        return newCoinLog;
-    }
-
 }
