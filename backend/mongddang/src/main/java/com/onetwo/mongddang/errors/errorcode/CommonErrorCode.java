cc27a4d34fd317b3f2841cad93b0fbdd8385d6a0
변서원 <qustjdnjs@naver.com>
2024-11-01 08:09:13 +0900

    :sparkles: Feat/(#44,S11P31D207-165) : 공통 ErrorCode 작성
    - 팀 코드에 맞게 코드 추가 및 필요없는 정보 삭제

--- backend/mongddang/src/main/java/com/onetwo/mongddang/errors/errorcode/CommonErrorCode.java
+++ backend/mongddang/src/main/java/com/onetwo/mongddang/errors/errorcode/CommonErrorCode.java
@@ -8,12 +8,13 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum CommonErrorCode implements ErrorCode {
 
-    INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "Invalid parameter included"),
-    RESOURCE_NOT_FOUND(HttpStatus.NOT_FOUND, "Resource not exists"),
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error"),
-    INVALID_QUERY_PARAMETER(HttpStatus.BAD_REQUEST, "Query parameter is invalid"),
+    INVALID_PARAMETER(HttpStatus.BAD_REQUEST,"G000","Invalid parameter included"),
+    RESOURCE_NOT_FOUND(HttpStatus.NOT_FOUND, "G001","Resource not exists"),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,"G002", "Internal server error"),
+    INVALID_QUERY_PARAMETER(HttpStatus.BAD_REQUEST,"G003", "Query parameter is invalid"),
     ;
 
     private final HttpStatus httpStatus;
+    private final String code;
     private final String message;
 }
