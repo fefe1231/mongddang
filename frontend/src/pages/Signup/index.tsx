6f09ec7a3ddb09f7e88aead18d3728c6b13d2312
조선미 <tjs2194@naver.com>
2024-10-31 10:06:38 +0900

    ✨ Feat/(#40, S11P31D207-204): config 파일 수정

--- frontend/src/pages/Signup/index.tsx
+++ frontend/src/pages/Signup/index.tsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React from 'react';
+import { TopBar } from '@/shared/ui/TopBar';
+import { Typography } from '@/shared/ui/Typography';
 
 export const Signup = () => {
   return (
     <div>
-      회원관리 페이지
+      <TopBar
+        type="iconpage"
+        iconHandler={() => {
+          console.log('뒤로가기 버튼');
+        }}
+      >
+        회원가입
+      </TopBar>
+      <Typography color="dark" size="1" weight={500}>
+        어떤 분이 사용하시나요?
+      </Typography>
     </div>
-  )
-}
+  );
+};
