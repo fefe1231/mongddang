ccaea9e7008025eb14a2016667bef09d01b7374d
변서원 <qustjdnjs@naver.com>
2024-11-02 03:21:25 +0900

    :sparkles: Feat/(#54,S11P31D207-214) : 닉네임 중복 검사 기능 구현

--- backend/mongddang/src/main/java/com/onetwo/mongddang/domain/user/controller/AuthController.java
+++ backend/mongddang/src/main/java/com/onetwo/mongddang/domain/user/controller/AuthController.java
@@ -1,7 +1,9 @@
 package com.onetwo.mongddang.domain.user.controller;
 
 import com.onetwo.mongddang.common.responseDto.ResponseDto;
+import com.onetwo.mongddang.domain.user.dto.CheckNicknameRequestDto;
 import com.onetwo.mongddang.domain.user.dto.SignupRequestDto;
+import com.onetwo.mongddang.domain.user.service.CheckNicknameService;
 import com.onetwo.mongddang.domain.user.service.SignupService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -20,11 +22,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final SignupService signupService;
+    private final CheckNicknameService checkNicknameService;
 
     @PostMapping("/signup")
     @Operation(summary = "회원가입 API", description = "비회원 유저에 대해 회원가입을 진행합니다.")
     public ResponseEntity<ResponseDto> signup(@Valid @RequestBody SignupRequestDto signupRequestDto){
         ResponseDto response = signupService.signup(signupRequestDto);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/check-nickname")
+    @Operation(summary = "닉네임 중복검사 API", description = "닉네임 중복 및 유효성을 검사합니다.")
+    public ResponseEntity<ResponseDto> checknickname(@Valid @RequestBody CheckNicknameRequestDto checkNicknameRequestDto){
+        ResponseDto response = checkNicknameService.checkNickname(checkNicknameRequestDto);
         return ResponseEntity.ok(response);
     }
 }
