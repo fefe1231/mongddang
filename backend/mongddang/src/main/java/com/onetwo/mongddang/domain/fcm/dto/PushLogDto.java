a476f62d408cf5886cbb794a8cb8f440f52ffd56
변서원 <qustjdnjs@naver.com>
2024-11-03 19:05:27 +0900

    :sparkles: Feat/(#68, S11P31D207-216) : 알림 기록 조회 기능 구현

---
+++ backend/mongddang/src/main/java/com/onetwo/mongddang/domain/fcm/dto/PushLogDto.java
@@ -0,0 +1,15 @@
+package com.onetwo.mongddang.domain.fcm.dto;
+
+import com.onetwo.mongddang.domain.fcm.model.PushLog;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Builder
+@Getter
+public class PushLogDto {
+    private PushLog.Category category;
+    private String content;
+    private LocalDateTime createdAt;
+}
---
+++ backend/mongddang/src/main/java/com/onetwo/mongddang/domain/fcm/repository/PushLogRepository.java
@@ -0,0 +1,12 @@
+package com.onetwo.mongddang.domain.fcm.repository;
+
+import com.onetwo.mongddang.domain.fcm.model.PushLog;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PushLogRepository extends JpaRepository<PushLog, Long> {
+    Page<PushLog> findAllByUserId(Long userId, Pageable pageable);
+}
