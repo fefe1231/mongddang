536e5d6d0e5fbaaffc73cfdaf567c429d0a33af5
조선미 <tjs2194@naver.com>
2024-11-04 12:17:37 +0900

    ✨ Feat/(#77, S11P31D207-229): 로그인 api 연결

---
+++ frontend/src/pages/login/api.ts
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+export const api = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  withCredentials: true,
+});
+
+// 회원정보 닉네임 수정
+export const login = (idToken: string) => {
+  return api({
+    url: '/api/auth/login',
+    method: 'post',
+    data: {
+      idToken: idToken,
+    },
+  });
+};
