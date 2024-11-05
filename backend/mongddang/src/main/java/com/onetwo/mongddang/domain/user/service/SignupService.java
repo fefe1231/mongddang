f4a7bf140f3b797aceaf759101b7685da21f19c4
변서원 <qustjdnjs@naver.com>
2024-11-05 10:37:59 +0900

    :sparkles: Feat/(#86, S11P31D207-255) : 회원가입 시 1번 몽땅/칭호 지급

--- backend/mongddang/src/main/java/com/onetwo/mongddang/domain/user/service/SignupService.java
+++ backend/mongddang/src/main/java/com/onetwo/mongddang/domain/user/service/SignupService.java
@@ -5,6 +5,14 @@ import com.onetwo.mongddang.domain.game.coinLog.application.CoinLogUtils;
 import com.onetwo.mongddang.domain.game.coinLog.model.CoinLog;
 import com.onetwo.mongddang.domain.game.coinLog.repository.CoinLogRepository;
 import com.onetwo.mongddang.domain.game.gameLog.application.GameLogUtils;
+import com.onetwo.mongddang.domain.game.mongddang.model.Mongddang;
+import com.onetwo.mongddang.domain.game.mongddang.model.MyMongddang;
+import com.onetwo.mongddang.domain.game.mongddang.repository.MongddangRepository;
+import com.onetwo.mongddang.domain.game.mongddang.repository.MyMongddangRepository;
+import com.onetwo.mongddang.domain.game.title.model.MyTitle;
+import com.onetwo.mongddang.domain.game.title.model.Title;
+import com.onetwo.mongddang.domain.game.title.repository.MyTitleRepository;
+import com.onetwo.mongddang.domain.game.title.repository.TitleRepository;
 import com.onetwo.mongddang.domain.user.dto.SignupRequestDto;
 import com.onetwo.mongddang.domain.user.error.CustomUserErrorCode;
 import com.onetwo.mongddang.domain.user.jwt.JwtTokenProvider;
@@ -17,8 +25,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -32,9 +42,13 @@ public class SignupService {
     private final GameLogUtils gameLogUtils;
     private final CoinLogUtils coinLogUtils;
     private final CoinLogRepository coinLogRepository;
+    private final MongddangRepository mongddangRepository;
+    private final TitleRepository titleRepository;
+    private final MyMongddangRepository myMongddangRepository;
+    private final MyTitleRepository myTitleRepository;
 
     @Transactional
-    public ResponseDto signup(SignupRequestDto signupRequestDto){
+    public ResponseDto signup(SignupRequestDto signupRequestDto) {
 
         // idtoken에서 회원 정보 뽑기(email)
         String idToken = signupRequestDto.getIdToken();
@@ -54,6 +68,10 @@ public class SignupService {
             initializeGameLog(newUser.getId());
             // 초기 재화 지급
             coinGrant(newUser);
+            //초기 몽땅
+            myFirstMongddang(newUser);
+            //초기 칭호
+            myFirstTitle(newUser);
         }
 
         // jwt token 생성
@@ -72,13 +90,13 @@ public class SignupService {
 
     // 회원가입
     @Transactional
-    public User signup(SignupRequestDto signupRequestDto,String email){
+    public User signup(SignupRequestDto signupRequestDto, String email) {
         User.Role role = signupRequestDto.getRole();
         String invitedCode = null; // 어른은 초대코드 null
 
         if ("child".equals(role.toString())) {
             // 어린이일 때 초대코드 생성
-            invitedCode =  makeRandomCodeService.makeCodeAndCheckUnique();
+            invitedCode = makeRandomCodeService.makeCodeAndCheckUnique();
         }
         // 저장
         User user = User.builder()
@@ -109,5 +127,37 @@ public class SignupService {
                 .category(CoinLog.CoinCategory.achievement)
                 .build();
         coinLogRepository.save(newCoinLog);
+    }
+
+    // 초기 몽땅 지급
+    @Transactional
+    public void myFirstMongddang(User user) {
+        // 1번 몽땅 가지고 오기
+        Optional<Mongddang> mongddang = mongddangRepository.findById(1L);
+
+        MyMongddang mymongddang = MyMongddang.builder()
+                .child(user)
+                .mongddang(mongddang.get())
+                .createdAt(LocalDateTime.now())
+                .isMain(true)
+                .isNew(true)
+                .build();
+        myMongddangRepository.save(mymongddang);
+    }
+
+    // 초기 칭호 지급
+    @Transactional
+    public void myFirstTitle(User user) {
+        // 1번 칭호 가져오기
+        Optional<Title> title = titleRepository.findById(1L);
+
+        MyTitle myTitle = MyTitle.builder()
+                .child(user)
+                .title(title.get())
+                .createdAt(LocalDateTime.now())
+                .isMain(true)
+                .isNew(true)
+                .build();
+        myTitleRepository.save(myTitle);
     }
 }
