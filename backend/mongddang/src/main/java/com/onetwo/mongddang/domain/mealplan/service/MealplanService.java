3ec0032c803b8d4ab5643b8c06a0ec8559077bcc
변서원 <qustjdnjs@naver.com>
2024-11-07 23:02:03 +0900

    :sparkles: Feat/(#96, S11P31D207-258) : 나이스 급식 api 조회 기능 구현
    - 학교 정보 조회 api
    - 급식 api

---
+++ backend/mongddang/src/main/java/com/onetwo/mongddang/domain/mealplan/service/MealplanService.java
@@ -0,0 +1,156 @@
+package com.onetwo.mongddang.domain.mealplan.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.onetwo.mongddang.common.responseDto.ResponseDto;
+import com.onetwo.mongddang.domain.mealplan.dto.RequestMealInfoDto;
+import com.onetwo.mongddang.domain.user.error.CustomCtoPErrorCode;
+import com.onetwo.mongddang.domain.user.error.CustomUserErrorCode;
+import com.onetwo.mongddang.domain.user.model.User;
+import com.onetwo.mongddang.domain.user.repository.CtoPRepository;
+import com.onetwo.mongddang.domain.user.repository.UserRepository;
+import com.onetwo.mongddang.errors.exception.RestApiException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class MealplanService {
+
+    private final UserRepository userRepository;
+    private final CtoPRepository ctoPRepository;
+
+    @Value("${school.key}")
+    private String schoolKey;
+
+    @Value("${school.url}")
+    private String schoolUrl;
+
+    @Value("${meal.key}")
+    private String mealKey;
+
+    @Value("${meal.url}")
+    private String mealUrl;
+
+    public ResponseDto getMealplan(RequestMealInfoDto requestMealInfoDto, Long userId) throws JsonProcessingException {
+        // user 있는지 확인
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RestApiException(CustomUserErrorCode.USER_NOT_FOUND));
+        log.info("Existing User : {}", userId);
+
+        // user가 보호자라면 닉네임 어린이와 연결되어있는지 확인
+        if (user.getRole() == User.Role.protector) {
+            log.info("this user is protector");
+
+            // 닉네임으로 어린이 찾기
+            User child = userRepository.findByNickname(requestMealInfoDto.getNickname())
+                    .orElseThrow(() -> new RestApiException(CustomUserErrorCode.THIS_NICKNAME_USER_NOT_FOUND));
+
+            //연결 여부 검사
+            if (!ctoPRepository.existsByChildAndProtector(user, child)) {
+                throw new RestApiException(CustomCtoPErrorCode.CHILD_NOT_LINKED);
+            }
+        }
+
+        // 학교 이름으로 학교 코드 / 교육청 코드 뽑기 (공공 api 사용)
+        JsonNode schoolInfo = getSchoolInfo(requestMealInfoDto.getSchoolName());
+        log.info("학교 코드 추출 완료!!");
+        log.info(schoolInfo.toString());
+
+        String schoolCode = schoolInfo.get(0).get("SD_SCHUL_CODE").asText(); // 학교 코드
+        String officeCode = schoolInfo.get(0).get("ATPT_OFCDC_SC_CODE").asText(); // 교육청 코드
+
+        // 연월 정보로 시작 시간, 종료 시간 yyyymmdd 모양으로 뽑기
+        YearMonth month = requestMealInfoDto.getMonth();
+
+        // 2024-11-01을 20241101 형식으로 바꾸는 포매터
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+
+        String startDay = month.atDay(1).format(formatter);
+        String endDay = month.atEndOfMonth().format(formatter);
+
+        // 조증석식 코드로 변환(조식 : 1 / 중식 : 2 / 석식 : 3)
+        String mealTimeCode = null;
+        switch (requestMealInfoDto.getMealTime()) {
+            case RequestMealInfoDto.MealType.breakfast:
+                mealTimeCode = "1";
+                break;
+            case RequestMealInfoDto.MealType.lunch:
+                mealTimeCode = "2";
+                break;
+            case RequestMealInfoDto.MealType.dinner:
+                mealTimeCode = "3";
+                break;
+        }
+
+        // 학교 코드, 교육청 코드, 식사시간, 시작시간, 종료 시간 넣어서 급식 식단 뽑기 (공공 api사용)
+        JsonNode mealInfo = getMeal(schoolCode, officeCode, mealTimeCode, startDay, endDay);
+        log.info("mealInfo : {}", mealInfo.get(0).toString());
+
+        //식단 반환
+        ResponseDto response = ResponseDto.builder()
+                .message("식단 조회를 성공했습니다.")
+                .data(mealInfo)
+                .build();
+
+        return response;
+    }
+
+    // 학교,교육청 api 코드 받아오기
+    public JsonNode getSchoolInfo(String schoolname) throws JsonProcessingException {
+        log.info("start getSchoolInfo");
+        String apiKey = schoolKey; // 발급받은 API Key를 여기에 입력하세요.
+        String apiUrl = schoolUrl + "?KEY=" + apiKey + "&Type=json&SCHUL_NM=" + schoolname; // 학교명으로 검색
+
+        RestTemplate restTemplate = new RestTemplate();
+        String response = restTemplate.getForObject(apiUrl, String.class);
+
+        // JSON 데이터를 파싱
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode rootNode = objectMapper.readTree(response);
+
+        // "schoolInfo"의 "row" 데이터만 반환
+        if (rootNode.has("schoolInfo") && rootNode.get("schoolInfo").size() > 1) {
+            JsonNode rowNode = rootNode.get("schoolInfo").get(1).path("row");
+            return rowNode;
+        } else {
+            return null;
+        }
+    }
+
+    // 급식표 가져오기
+    public JsonNode getMeal(String schoolCode, String officeCode,
+                            String mealTimeCode, String startDay,
+                            String endDay) throws JsonProcessingException {
+        String apiKey = mealKey; // 발급받은 API Key를 여기에 입력하세요.
+        String apiUrl = mealUrl + "?KEY=" + apiKey + "&Type=json&pIndex=1&pSize=100&ATPT_OFCDC_SC_CODE=" + officeCode
+                + "&SD_SCHUL_CODE=" + schoolCode + "&MLSV_FROM_YMD=" + startDay
+                + "&MLSV_TO_YMD=" + endDay + "&MMEAL_SC_CODE=" + mealTimeCode;
+
+        log.info("meal api Url : {}", apiUrl);
+
+        RestTemplate restTemplate = new RestTemplate();
+        String response = restTemplate.getForObject(apiUrl, String.class);
+
+        // JSON 데이터를 파싱
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode rootNode = objectMapper.readTree(response);
+
+        // "schoolInfo"의 "row" 데이터만 반환
+        if (rootNode.has("mealServiceDietInfo") && rootNode.get("mealServiceDietInfo").size() > 1) {
+            JsonNode rowNode = rootNode.get("mealServiceDietInfo").get(1).path("row");
+            return rowNode;
+        } else {
+            return null;
+        }
+
+    }
+}
--- backend/mongddang/src/main/java/com/onetwo/mongddang/domain/user/repository/CtoPRepository.java
+++ backend/mongddang/src/main/java/com/onetwo/mongddang/domain/user/repository/CtoPRepository.java
@@ -7,13 +7,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface CtoPRepository extends JpaRepository<CtoP, Long> {
 
     // 보호자와 어린이가 함께 엮인 데이터를 조회합니다. 그저 존재여부를 찾기 위함임.
     @Query("SELECT c FROM CtoP c WHERE c.child = :child AND c.protector = :protector")
-    List<CtoP> findByChildAndProtector(@Param("protector") User protector,@Param("child") User child);
+    List<CtoP> findByChildAndProtector(@Param("protector") User protector, @Param("child") User child);
 
     // 보호자 id를 통해, 연결된 어린이 정보들을 리스트 형식으로 조회합니다.
     @Query("SELECT c.child FROM CtoP c WHERE c.protector.id = :protectorId")
@@ -22,4 +21,6 @@ public interface CtoPRepository extends JpaRepository<CtoP, Long> {
     // 어린이 id를 통해, 연결된 보호자 정보들을 리스트 형식으로 조회합니다.
     @Query("SELECT c.protector FROM CtoP c WHERE c.child.id = :childId")
     List<User> findProtectorByChildId(@Param("childId") Long childId);
+
+    Boolean existsByChildAndProtector(User child, User protector);
 }
