83e585729cf5bf96537d7cc2eee39fa568389118
권대호 <kown9512@gmail.com>
2024-11-07 15:25:05 +0900

    ✨ Feat/(#91, S11P31D207-238): char, calendar 추가

--- frontend/src/pages/record/ui/record-page.tsx
+++ frontend/src/pages/record/ui/record-page.tsx
@@ -1,8 +1,32 @@
 import { TopBar } from '@/shared/ui/TopBar';
+import { useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { LineChart, LineChartSeries } from '@mantine/charts';
+import { data } from './data.ts';
+import { DatePicker } from '@mantine/dates';
+import { ScrollArea } from '@mantine/core';
 
 export const RecordPage = () => {
   const nav = useNavigate();
+
+  const [value, setValue] = useState<Date[]>([]);
+  const viewportRef = useRef<HTMLDivElement>(null);
+
+  const checkMeal = (series: LineChartSeries) => ({
+    dot:
+      series.name === 'BeforeMeal' || series.name === 'AfterMeal'
+        ? { r: 6 }
+        : {},
+  });
+
+  useEffect(() => {
+    if (viewportRef.current) {
+      viewportRef.current.scrollTo({
+        left: viewportRef.current.scrollWidth,
+        behavior: 'smooth',
+      });
+    }
+  }, []);
   return (
     <>
       <header>
@@ -10,6 +34,29 @@ export const RecordPage = () => {
           내 기록
         </TopBar>
       </header>
+      {/* Calendar start */}
+      <DatePicker type="multiple" value={value} onChange={setValue} />
+      {/* Calendar end */}
+
+      {/* LineChart start */}
+      <ScrollArea w={700} h={350} viewportRef={viewportRef}>
+        <LineChart
+          h={300}
+          w={1000}
+          data={data}
+          dataKey="date"
+          series={[
+            { name: 'Glucose', color: 'indigo.6' },
+            { name: 'BeforeMeal', color: 'orange.9' },
+            { name: 'AfterMeal', color: 'orange.9' },
+          ]}
+          dotProps={{ r: 6, strokeWidth: 2, stroke: '#fff' }}
+          curveType="linear"
+          lineProps={checkMeal}
+          style={{ margin: '10px' }}
+        />
+      </ScrollArea>
+      {/* LineChart end */}
       <div>record page</div>
     </>
   );
