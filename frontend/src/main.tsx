73f186dcd13f0f3a8ef682e1735f537a61c736b0
권대호 <kown9512@gmail.com>
2024-11-07 15:32:11 +0900

    ✨ Feat/(#91, S11P31D207-238): Mantine Provider 및 DatesProvider 추가

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,13 +3,29 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { MantineProvider, createTheme } from '@mantine/core';
+import { DatesProvider } from '@mantine/dates';
+import '@mantine/core/styles.css';
+import 'dayjs/locale/ko';
+import '@mantine/charts/styles.css';
+import '@mantine/dates/styles.css';
 
 const queryClient = new QueryClient();
+
+const theme = createTheme({
+  primaryColor: 'cyan',
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <MantineProvider theme={theme}>
+        <DatesProvider
+          settings={{ locale: 'ko', firstDayOfWeek: 0, weekendDays: [0] }}
+        >
+          <App />
+        </DatesProvider>
+      </MantineProvider>
     </QueryClientProvider>
   </StrictMode>
 );
