75e3934e521d2df71d5ede9eac8b95368dca538f
권대호 <kown9512@gmail.com>
2024-11-07 17:58:41 +0900

    ✨ Feat/(#91, S11P31D207-238): 일자 포맷 커스텀 함수 작성중

--- frontend/src/pages/record/ui/record-page.tsx
+++ frontend/src/pages/record/ui/record-page.tsx
@@ -1,24 +1,67 @@
 import { TopBar } from '@/shared/ui/TopBar';
 import { useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { LineChart, LineChartSeries } from '@mantine/charts';
-import { data } from './data.ts';
-import { DatePicker } from '@mantine/dates';
-import { ScrollArea } from '@mantine/core';
+import { DatePicker, DatePickerProps } from '@mantine/dates';
+import { Center } from '@mantine/core';
+import dayjs from 'dayjs';
+// import { LineChart, LineChartSeries } from '@mantine/charts';
+// import { ScrollArea } from '@mantine/core';
+// import axios from 'axios';
+
+// type ChartData = {
+//   date: string;
+//   Glucose: number;
+//   Oranges: number;
+//   Tomatoes: number;
+// };
+
+// type Data = 'data';
+
+// const fetchGlucoseData = async () => {
+//   try {
+//     const res = await axios.get('/frontend/public/dummy/glucose-data.json');
+//     return res.data.json();
+//   } catch (err) {
+//     console.log(err);
+//     throw err;
+//   }
+// };
+
+const setDayProps: DatePickerProps['getDayProps'] = (date) => {
+  const formattedDay = String(dayjs().toDate().getDate()).padStart(2, '0');
+
+  if (dayjs(date).format('YYYY-MM-DD') === dayjs().format('YYYY-MM-DD')) {
+    return {
+      style: {
+        textDecoration: 'underline',
+      },
+      children: formattedDay,
+    };
+  }
+
+  return {
+    children: formattedDay,
+  };
+};
 
 export const RecordPage = () => {
   const nav = useNavigate();
 
-  const [value, setValue] = useState<Date[]>([]);
+  // const [data, setData] = useState<Record<Data, ChartData>[]>([]);
+
+  const [dateValue, setDateValue] = useState<Date | null>(null);
   const viewportRef = useRef<HTMLDivElement>(null);
 
-  const checkMeal = (series: LineChartSeries) => ({
-    dot:
-      series.name === 'BeforeMeal' || series.name === 'AfterMeal'
-        ? { r: 6 }
-        : {},
-  });
+  // const checkMeal = (series: LineChartSeries) => ({
+  //   dot:
+  //     series.name === 'BeforeMeal' || series.name === 'AfterMeal'
+  //       ? { r: 6 }
+  //       : {},
+  // });
 
+  const handleDoubleClick = (date: Date) => {};
+
+  // mount 시 linechart의 가장 오른쪽으로 이동
   useEffect(() => {
     if (viewportRef.current) {
       viewportRef.current.scrollTo({
@@ -27,6 +70,16 @@ export const RecordPage = () => {
       });
     }
   }, []);
+
+  // chart data 저장
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     const data = await fetchGlucoseData();
+  //     setData(data);
+  //   };
+  //   getData();
+  // }, []);
+
   return (
     <>
       <header>
@@ -35,11 +88,19 @@ export const RecordPage = () => {
         </TopBar>
       </header>
       {/* Calendar start */}
-      <DatePicker type="multiple" value={value} onChange={setValue} />
+      <Center>
+        <DatePicker
+          value={dateValue}
+          onChange={setDateValue}
+          type="default"
+          size="lg"
+          getDayProps={setDayProps}
+        />
+      </Center>
       {/* Calendar end */}
 
       {/* LineChart start */}
-      <ScrollArea w={700} h={350} viewportRef={viewportRef}>
+      {/* <ScrollArea w={700} h={350} viewportRef={viewportRef}>
         <LineChart
           h={300}
           w={1000}
@@ -55,7 +116,7 @@ export const RecordPage = () => {
           lineProps={checkMeal}
           style={{ margin: '10px' }}
         />
-      </ScrollArea>
+      </ScrollArea> */}
       {/* LineChart end */}
       <div>record page</div>
     </>
