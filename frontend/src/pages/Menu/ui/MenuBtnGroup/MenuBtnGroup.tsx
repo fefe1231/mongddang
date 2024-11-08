981251470225be71a90470151dbde575d79b65c5
김민진 <kminjin99@naver.com>
2024-11-09 03:27:07 +0900

    ✨ Feat/(#102, S11P31D207-279) : 메뉴페이지 url 연결
    .

--- frontend/src/pages/Menu/ui/MenuBtnGroup/MenuBtnGroup.tsx
+++ frontend/src/pages/Menu/ui/MenuBtnGroup/MenuBtnGroup.tsx
@@ -3,14 +3,21 @@
 import { IconTypo } from '@/shared/ui/IconTypo';
 import { btn, btnGroup } from './MenuBtnGroup.styles';
 import { menuBtnData } from '../../constants/menuBtnData';
-
+import { useNavigate } from 'react-router-dom';
 
 const MenuBtnGroup = () => {
+  const navigate = useNavigate();
   return (
     <div css={btnGroup}>
-      {menuBtnData.map((item) => {
+      {menuBtnData.map((item, i) => {
         return (
-          <div css={btn}>
+          <div
+            css={btn}
+            onClick={() => {
+              navigate(item.urlTo);
+            }}
+            key={`menuBtn-${i}`}
+          >
             <IconTypo icon={item.icon} menu={item.menu} />
           </div>
         );
