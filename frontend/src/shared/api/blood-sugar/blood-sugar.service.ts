de506dbfa6c49d83739b3ee41e7fee51be4acd58
권대호 <kown9512@gmail.com>
2024-11-11 17:58:10 +0900

    🐛 Fix/(#91, S11P31D207-237): 혈당 서비스 응답 데이터 타입 수정

--- frontend/src/shared/api/blood-sugar/blood-sugar.service.ts
+++ frontend/src/shared/api/blood-sugar/blood-sugar.service.ts
@@ -1,9 +1,12 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import { api } from '../interceptors';
 import { BloodsugarResponse } from './blood-sugar.type';
 
 export class BloodsugarService {
-  static bloodSugarQuery(nickname: string, date: string): Promise<BloodsugarResponse> {
+  static bloodSugarQuery(
+    nickname: string,
+    date: string
+  ): Promise<AxiosResponse<BloodsugarResponse>> {
     return nickname === 'test-chart-data'
       ? axios.get('/public/dummy/blood-sugar.json')
       : api.get(`/api/bloodsuger/${nickname}/${date}`);
