9c0eba340465f5146fd3462f36dc8d66e79d7985
변서원 <qustjdnjs@naver.com>
2024-11-11 16:17:59 +0900

    :sparkles: Feat/(#103, S11P31D207-322,S11P31D207-320): 복약 알림 기능 구현

--- backend/mongddang/src/main/java/com/onetwo/mongddang/domain/record/repository/RecordRepository.java
+++ backend/mongddang/src/main/java/com/onetwo/mongddang/domain/record/repository/RecordRepository.java
@@ -4,6 +4,8 @@ import com.onetwo.mongddang.domain.record.model.Record;
 import com.onetwo.mongddang.domain.record.model.Record.RecordCategoryType;
 import com.onetwo.mongddang.domain.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -15,6 +17,10 @@ public interface RecordRepository extends JpaRepository<Record, Long> {
     Optional<Record> findTopByChildAndCategoryAndEndTimeIsNullOrderByIdDesc(User child, RecordCategoryType recordCategoryType);
 
     List<Record> findByChildAndStartTimeBetween(User child, LocalDateTime startDate, LocalDateTime endDate);
+
+    @Query("SELECT r FROM Record r WHERE r.category = :category AND r.startTime = :startTime")
+    List<Record> findByCategoryAndStartTime(@Param("category") Record.RecordCategoryType category,
+                                            @Param("startTime") LocalDateTime startTime);
 
 //    List<Record> findByChildIdAndStartTimeBetween(Long childId, LocalDateTime todayStart, LocalDateTime todayEnd);
 }
--- backend/mongddang/src/main/java/com/onetwo/mongddang/domain/vital/repository/VitalRepository.java
+++ backend/mongddang/src/main/java/com/onetwo/mongddang/domain/vital/repository/VitalRepository.java
@@ -14,10 +14,10 @@ public interface VitalRepository extends JpaRepository<Vital,Long> {
     @Query("SELECT e FROM Vital e WHERE function('date', e.measurementTime) = :date AND e.child.id = :childId")
     List<Vital> findAllByDateAndChildId(@Param("date") LocalDate date, @Param("childId") Long childId);
 
-    @Query("SELECT v FROM Vital v WHERE v.measurementTime BETWEEN :start AND :end AND v.status IN (Vital.GlucoseStatusType.low, Vital.GlucoseStatusType.high)")
+    @Query("SELECT v FROM Vital v WHERE v.measurementTime BETWEEN :start AND :end AND v.status IN (com.onetwo.mongddang.domain.vital.model.Vital.GlucoseStatusType.low, com.onetwo.mongddang.domain.vital.model.Vital.GlucoseStatusType.high)")
     List<Vital> findByMeasurementTimeBetween(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end);
 
-    @Query("SELECT v.child FROM Vital v WHERE v.measurementTime BETWEEN :start AND :end AND v.status IN (Vital.GlucoseStatusType.low, Vital.GlucoseStatusType.high) " +
+    @Query("SELECT v.child FROM Vital v WHERE v.measurementTime BETWEEN :start AND :end AND v.status IN (com.onetwo.mongddang.domain.vital.model.Vital.GlucoseStatusType.low, com.onetwo.mongddang.domain.vital.model.Vital.GlucoseStatusType.high) " +
             "GROUP BY v.child HAVING COUNT(v) >= 3")
     List<Vital> findPersistentAbnormalVitals(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end);
 }
