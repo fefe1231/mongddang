ad1b3342c60a1efd752b2a3608d17d610947f542
vaaast-lake <kown9512@gmail.com>
2024-11-11 23:02:27 +0900

    ✨ Feat/(#91, S11P31D207-237): tooltip 애니메이션 추가

--- frontend/src/widgets/blood-sugar-chart/ui/blood-sugar-chart-ui.tsx
+++ frontend/src/widgets/blood-sugar-chart/ui/blood-sugar-chart-ui.tsx
@@ -54,10 +54,6 @@ export const BloodSugarChart = () => {
             data={data}
             dataKey="measurementTime"
             curveType="bump"
-            // type="gradient"
-            // gradientStops={[
-            //   { offset: 0, color: 'gray.6' },
-            // ]}
             strokeWidth={3}
             withLegend
             legendProps={{ verticalAlign: 'bottom', height: 50 }}
@@ -68,17 +64,21 @@ export const BloodSugarChart = () => {
                 color: 'indigo.6',
               },
             ]}
-            // withDots={false}
             dotProps={{ r: 4, strokeWidth: 1.5, stroke: '#fff' }}
             activeDotProps={{ r: 6, strokeWidth: 1.5, fill: '#fff' }}
             yAxisProps={{ domain: [30, 250] }}
             strokeDasharray="10 10"
-            tooltipAnimationDuration={300}
             referenceLines={[
               { y: 50, label: '저혈당 위험', color: 'red.6' },
               { y: 140, label: '고혈당 위험', color: 'yellow.5' },
             ]}
             style={{ margin: '30px' }}
+            tooltipProps={{
+              active: undefined,
+              isAnimationActive: true,
+              animationEasing: 'ease-in-out',
+              animationDuration: 300,
+            }}
           />
         </ScrollArea>
       )}
