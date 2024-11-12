22508c30a97831b9791c74792c12801e4b0ea4fe
권대호 <kown9512@gmail.com>
2024-11-12 10:11:46 +0900

    ✨ Feat/(#91, S11P31D207-359): 탭 메뉴 적용

--- frontend/src/pages/today-record/ui/TodayRecord.tsx
+++ frontend/src/pages/today-record/ui/TodayRecord.tsx
@@ -1,16 +1,29 @@
 import { BloodSugarChart } from '@/widgets/blood-sugar-chart/ui';
 import { TopBar } from '@/shared/ui/TopBar';
 import { useNavigate } from 'react-router-dom';
+import { TabMenu } from '@/shared/ui/TabMenu/indes';
 
 export const TodayRecordPage = () => {
   const nav = useNavigate();
-
+  const handleTabChange = (tabId: string) => {
+    console.log(tabId);
+  };
   return (
     <>
-      <TopBar type="iconpage" iconHandler={() => nav(-1)}>
-        내 기록
-      </TopBar>
-      <BloodSugarChart />
+      <header>
+        <TopBar type="iconpage" iconHandler={() => nav(-1)}>
+          내 기록
+        </TopBar>
+      </header>
+      <section>
+        <BloodSugarChart />
+      </section>
+      <section>
+        <TabMenu
+          contents={[<div>1</div>, <div>2</div>, <div>3</div>, <div>4</div>]}
+          onTabChange={handleTabChange}
+        />
+      </section>
     </>
   );
 };
