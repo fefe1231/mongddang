a4842117096b48eb4a97857304bd7698835d605d
조선미 <tjs2194@naver.com>
2024-11-12 15:27:04 +0900

    🐛 Fix/(#108, S11P31D207-309): 상세보기 컴포넌트 수정

--- frontend/src/pages/report/ui/slider-item/index.tsx
+++ frontend/src/pages/report/ui/slider-item/index.tsx
@@ -1,9 +1,10 @@
 /** @jsxImportSource @emotion/react */
-
 import { Typography } from '@/shared/ui/Typography';
 import { base } from '../item/stylest';
 import { Slider } from '@/shared/ui/Slider';
 import { SliderCss, containerCss } from './styles';
+import { Icon } from '@/shared/ui/Icon';
+import { HiOutlineChevronRight } from 'react-icons/hi';
 
 interface ItemProps {
   title: string;
@@ -14,9 +15,31 @@ interface ItemProps {
 export const SliderItem = ({ title, ment, unit }: ItemProps) => {
   return (
     <div css={base}>
-      <Typography color="dark" size="1" weight={500}>
-        {title}
-      </Typography>
+      <div 
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          width: '100%'
+        }}
+      >
+        <Typography color="dark" size="1" weight={500}>
+          {title}
+        </Typography>
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+          }}
+        >
+          <Typography color="dark" size="1" weight={500}>
+            더보기
+          </Typography>
+          <Icon size={1.5}>
+            <HiOutlineChevronRight />
+          </Icon>
+        </div>
+      </div>
       <div css={containerCss}>
         <div style={{ display: 'flex', alignItems: 'flex-end' }}>
           <Typography color="dark" size="1.25" weight={600}>
@@ -26,16 +49,16 @@ export const SliderItem = ({ title, ment, unit }: ItemProps) => {
             color="dark"
             size="0.75"
             weight={500}
-            style={{ marginLeft: '4px', marginBottom: '2px' }} // 아래로 살짝 이동
+            style={{ marginLeft: '4px', marginBottom: '2px' }}
           >
             {unit}
           </Typography>
         </div>
         <Slider
           css={SliderCss}
-          blueMent="'권장'"
+          blueMent="권장"
           max={50}
-          redMent="'주의'"
+          redMent="주의"
           standard={30}
           value={30}
         />
