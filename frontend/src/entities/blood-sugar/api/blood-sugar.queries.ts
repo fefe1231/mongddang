9e0d52b816f287ec84fa6b1e7de69bf4ba3b792a
권대호 <kown9512@gmail.com>
2024-11-12 13:35:58 +0900

    ♻️ Refactor/(#91, S11P31D207-359): 불필요 로그 삭제

--- frontend/src/entities/blood-sugar/api/blood-sugar.queries.ts
+++ frontend/src/entities/blood-sugar/api/blood-sugar.queries.ts
@@ -19,14 +19,10 @@ export class BloodsugarQueries {
           nickname,
           date
         );
-        console.log('formatting');
-
         const formattedData = data.data.bloodSugar.map((item) => ({
           ...item,
           measurementTime: dayjs(item.measurementTime).format('HH:mm'),
         }));
-        console.log('formattedData', formattedData);
-
         return formattedData;
       },
       enabled: !!nickname,
