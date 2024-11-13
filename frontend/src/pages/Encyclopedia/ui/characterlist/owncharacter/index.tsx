6e355e2d72b71886d121b9be8429045389e48246
조선미 <tjs2194@naver.com>
2024-11-13 13:59:55 +0900

    ✨ Feat/(#125, S11P31D207-312): 캐릭터 도감 메인 설정

--- frontend/src/pages/Encyclopedia/ui/characterlist/owncharacter/index.tsx
+++ frontend/src/pages/Encyclopedia/ui/characterlist/owncharacter/index.tsx
@@ -7,6 +7,8 @@ import {
   formatId,
 } from '@/pages/Encyclopedia/model/mongddang-img';
 import { Icon } from '@/shared/ui/Icon';
+import star from '../../../../../assets/img/icon/main_star.png';
+
 interface OwnCharacterProps {
   data: ICharacterData;
 }
@@ -15,15 +17,23 @@ export const Owncharacter = ({ data }: OwnCharacterProps) => {
   const imageKey = formatId(data.id);
   const imagePath = characterImages[imageKey];
 
-  
   return (
     <>
       <div css={containerCss}>
-        <IconTypo fontSize="1" icon={imagePath} menu={data.name} size={5} />
-        <Icon size={3}>
-          <img alt="icon-0" src="/img/%EB%A7%90%EB%9E%911.png" />
-        </Icon>
+        <div style={{ position: 'relative', width: '100%', height: '100%' }}>
+          <IconTypo fontSize="1" icon={imagePath} menu={data.name} size={5} />
+          {data.isMain ? (
+            <Icon size={3} style={{ 
+              position: 'absolute', 
+              top: '-0.5rem', 
+              right: '-0.5rem'
+            }}>
+              <img alt="icon-0" src={star} />
+            </Icon>
+          ) : null}
+        </div>
       </div>
     </>
   );
 };
+
