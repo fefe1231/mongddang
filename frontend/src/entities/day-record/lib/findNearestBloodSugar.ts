00a0a0b9e881045a0d606c5acd8e9108142b2373
vaaast-lake <kown9512@gmail.com>
2024-11-14 01:08:55 +0900

    ♻️ Refactor/(#91, S11P31D207-359): 가까운 시간 혈당 찾는 함수 내부 함수명 수정

--- frontend/src/entities/day-record/lib/findNearestBloodSugar.ts
+++ frontend/src/entities/day-record/lib/findNearestBloodSugar.ts
@@ -5,24 +5,16 @@ export const findNearestTimeBloodSugar = (
   data: Bloodsugar[],
   targetTime: string
 ): number => {
-  const convertISOTimeToMinutesWithDayjs = (isoTime: string): number => {
-    const time = dayjs(isoTime);
-
-    console.log('m', time.minute());
-    console.log('h', time.hour());
-    console.log('all', time.hour() * 60 + time.minute());
-
-    return time.hour() * 60 + time.minute();
+  const convertTimeToMinutes = (time: string): number => {
+    const dayjsTime = dayjs(time);
+    return dayjsTime.hour() * 60 + dayjsTime.minute();
   };
 
-  // 두 시간 간의 차이를 분 단위로 계산하는 함수
   const getTimeDifference = (time1: number, time2: number): number => {
     return Math.abs(time1 - time2);
   };
 
-  // 타겟 시간을 분으로 변환
-  const targetMinutes = convertISOTimeToMinutesWithDayjs(targetTime);
-
+  const targetMinutes = convertTimeToMinutes(targetTime);
   let left = 0;
   let right = data.length - 1;
   let nearestEntry = data[0].bloodSugarLevel;
@@ -30,24 +22,19 @@ export const findNearestTimeBloodSugar = (
 
   while (left <= right) {
     const mid = Math.floor((left + right) / 2);
-    const currentMinutes = convertISOTimeToMinutesWithDayjs(
-      data[mid].measurementTime
-    );
+    const currentMinutes = convertTimeToMinutes(data[mid].measurementTime);
     const timeDiff = getTimeDifference(targetMinutes, currentMinutes);
 
-    // 현재 시간이 타겟 시간과의 차이가 더 작으면 업데이트
     if (timeDiff < minDiff) {
       minDiff = timeDiff;
       nearestEntry = data[mid].bloodSugarLevel;
     }
 
-    // 이진 탐색 진행
     if (currentMinutes < targetMinutes) {
       left = mid + 1;
     } else if (currentMinutes > targetMinutes) {
       right = mid - 1;
     } else {
-      // 정확히 일치하는 시간을 찾은 경우
       return data[mid].bloodSugarLevel;
     }
   }
