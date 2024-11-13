8761d863890b6592322ce5ea9df535d9ae9525d4
조선미 <tjs2194@naver.com>
2024-11-13 23:16:45 +0900

    ✨ Feat/(#126, S11P31D207-236): 전체, gmi 그래프 구현

--- frontend/src/pages/report/index.tsx
+++ frontend/src/pages/report/index.tsx
@@ -5,16 +5,37 @@ import { Item } from './ui/item';
 import { SliderItem } from './ui/slider-item';
 import { GptContent } from './ui/gpt-content';
 import { useNavigate } from 'react-router-dom';
+import { WeekChart } from './ui/chart/week-chart';
+import { useQuery } from '@tanstack/react-query';
+import { getreport } from './api/api';
 
 export const Report = () => {
   const nav = useNavigate();
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['bloodSugarReport'],
+    queryFn: getreport,
+  });
+
+  console.log(data?.data.data
+  )
+
+  if (isLoading) {
+    console.log('Loading data...');
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    console.log('Error:', error);
+    return <div>Error occurred!</div>;
+  }
   return (
     <div>
       <TopBar type="iconpage" iconHandler={()=>nav('/menu')}>주간 리포트</TopBar>
-      <Item title="혈당관리지표(GMI)" ment="내용" unit="%" />
-      <SliderItem title="이번주 평균 혈당" ment="내용" unit="%" url = '/report/detail/mean'/>
-      <SliderItem title="혈당 변동성" ment="내용" unit="mg/dl" url = '/report/detail/gv'/>
-      <SliderItem title="목표 범위 내 비율" ment="내용" unit="%" url = '/report/detail/tir'/>
+      <WeekChart data={data?.data.data.glucoseMeasurementItmeList}/>
+      <Item title="혈당관리지표(GMI)" ment={Number(data?.data.data.gmi).toFixed(1)} unit="%" />
+      <SliderItem title="이번주 평균 혈당" max={400} standard={100} ment={Number(data?.data.data.abg).toFixed(1)} unit="mg/dl" url = '/report/detail/mean'/>
+      <SliderItem title="혈당 변동성" max={100} standard={36} ment={Number(data?.data.data.cv).toFixed(1)} unit="%" url = '/report/detail/gv'/>
+      <SliderItem title="목표 범위 내 비율" max={100} standard={20} ment={Number(data?.data.data.tir).toFixed(1)} unit="%" url = '/report/detail/tir'/>
       <GptContent />
     </div>
   );
--- frontend/src/shared/api/interceptors.ts
+++ frontend/src/shared/api/interceptors.ts
@@ -12,10 +12,13 @@ export const api = axios.create({
 // 요청에 자동으로 accessToken 추가
 api.interceptors.request.use(async (config) => {
   // const accessToken = localStorage.getItem('accessToken');
-  const accessToken = (await PreferencesUser.getUser()).userToken;
+  // const accessToken = (await PreferencesUser.getUser()).userToken;
+  const accessToken = '';
+  console.log('액세스토큰 ',accessToken)
 
   if (accessToken) {
     config.headers.Authorization = `Bearer ${accessToken}`;
+    console.log('액세스토큰ㅂㅇㅁㄴ',accessToken)
   }
 
   return config;
