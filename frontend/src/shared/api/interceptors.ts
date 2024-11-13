d8442210efe18ebc56d9a792b1d46b90de455992
조선미 <tjs2194@naver.com>
2024-11-14 01:00:24 +0900

    ✨ Feat/(#126, S11P31D207-236): interceptor 복구

--- frontend/src/shared/api/interceptors.ts
+++ frontend/src/shared/api/interceptors.ts
@@ -12,13 +12,10 @@ export const api = axios.create({
 // 요청에 자동으로 accessToken 추가
 api.interceptors.request.use(async (config) => {
   // const accessToken = localStorage.getItem('accessToken');
-  // const accessToken = (await PreferencesUser.getUser()).userToken;
-  const accessToken = '';
-  console.log('액세스토큰 ',accessToken)
+  const accessToken = (await PreferencesUser.getUser()).userToken;
 
   if (accessToken) {
     config.headers.Authorization = `Bearer ${accessToken}`;
-    console.log('액세스토큰ㅂㅇㅁㄴ',accessToken)
   }
 
   return config;
