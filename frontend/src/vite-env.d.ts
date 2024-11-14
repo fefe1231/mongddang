600665c47ce2088df285ba32ed52e1bd22e7e631
권대호 <kown9512@gmail.com>
2024-11-14 12:44:52 +0900

    ✨ Feat/(#135, S11P31D207-367): web client id 추가

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,12 @@
+// 타입스크립트에서 import.meta.env를 제대로 쓰기 위해서는 다음과 같이 ImportMetaEnv와 ImportMeta 인터페이스를 정의해주어야 합니다.
+// https://khj0426.tistory.com/238
+// https://vitejs.dev/guide/env-and-mode
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_GOOGLE_WEB_CLIENT_ID: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
