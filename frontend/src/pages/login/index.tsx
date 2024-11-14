483fe6d75da55a4b5efd3aeff35f45d4c7d6b0b2
권대호 <kown9512@gmail.com>
2024-11-14 12:43:34 +0900

    ✨ Feat/(#135, S1131D207-367): 구글 로그인 기능 추가

--- frontend/src/pages/login/index.tsx
+++ frontend/src/pages/login/index.tsx
@@ -1,73 +1,88 @@
 /** @jsxImportSource @emotion/react */
-import { GoogleLogin } from '@react-oauth/google';
-import axios, { AxiosResponse } from 'axios';
-import { useNavigate } from 'react-router-dom';
+// import { GoogleLogin } from '@react-oauth/google';
+// import axios, { AxiosResponse } from 'axios';
+// import { useNavigate } from 'react-router-dom';
 import { base, contentCss, googleCss } from './ui/styles';
 import { Icon } from '@/shared/ui/Icon';
 import { Typography } from '@/shared/ui/Typography';
-import { useUserStore } from '@/entities/user/model';
-import { UserService } from '@/shared/api/user/user.service';
-import { LoginResponse } from './api/api';
+// import { useUserStore } from '@/entities/user/model';
+// import { UserService } from '@/shared/api/user/user.service';
+// import { LoginResponse } from './api/api';
+import { SocialLogin } from '@capgo/capacitor-social-login';
 
-interface IcredentialResponse {
-  credential?: string;
-  clientId?: string;
-  select_by?: string;
-}
+// interface IcredentialResponse {
+//   credential?: string;
+//   clientId?: string;
+//   select_by?: string;
+// }
 
 const Login = () => {
-  const nav = useNavigate();
+  // const nav = useNavigate();
 
-  const updateUser = useUserStore((state) => state.updateUser);
+  // const updateUser = useUserStore((state) => state.updateUser);
 
-  const handleLoginSuccess = (credentialResponse: IcredentialResponse) => {
-    const idToken = credentialResponse.credential;
-    console.log('ID Token:', idToken);
+  // const handleLoginSuccess = (credentialResponse: IcredentialResponse) => {
+  //   const idToken = credentialResponse.credential;
+  //   console.log('ID Token:', idToken);
 
-    // ID Token을 백엔드로 전송
-    axios
-      .post(
-        `${import.meta.env.VITE_API_BASE_URL}/api/auth/login`,
-        { idToken },
-        {
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${idToken}`,
-          },
-        }
-      )
-      .then(async (response: AxiosResponse<LoginResponse>) => {
-        // const accessToken = response.data.body.accessToken;
-        // console.log('토큰 저장 성공:', accessToken);
-        const userToken = response.data.data.accessToken;
+  //   // ID Token을 백엔드로 전송
+  //   axios
+  //     .post(
+  //       `${import.meta.env.VITE_API_BASE_URL}/api/auth/login`,
+  //       { idToken },
+  //       {
+  //         headers: {
+  //           'Content-Type': 'application/json',
+  //           Authorization: `Bearer ${idToken}`,
+  //         },
+  //       }
+  //     )
+  //     .then(async (response: AxiosResponse<LoginResponse>) => {
+  //       // const accessToken = response.data.body.accessToken;
+  //       // console.log('토큰 저장 성공:', accessToken);
+  //       const userToken = response.data.data.accessToken;
 
-        // accessToken을 로컬 스토리지에 저장
-        // localStorage.setItem('accessToken', accessToken);
+  //       // accessToken을 로컬 스토리지에 저장
+  //       // localStorage.setItem('accessToken', accessToken);
 
-        // accessToken 스토어 및 perference에 저장
-        await updateUser({ userToken });
-        // user 정보를 스토어 및 perference에 저장
-        const user = (await UserService.userQuery()).data.data;
-        await updateUser({ user });
+  //       // accessToken 스토어 및 perference에 저장
+  //       await updateUser({ userToken });
+  //       // user 정보를 스토어 및 perference에 저장
+  //       const user = (await UserService.userQuery()).data.data;
+  //       await updateUser({ user });
 
-        // accessToken을 axios 전역 헤더에 설정
-        axios.defaults.headers.common['Authorization'] = `Bearer ${userToken}`;
+  //       // accessToken을 axios 전역 헤더에 설정
+  //       axios.defaults.headers.common['Authorization'] = `Bearer ${userToken}`;
 
-        // 회원 여부에 따른 페이지 이동
-        if (response.data.data.isRegistered) {
-          nav('/');
-        } else {
-          nav('/signup', { state: { idToken } });
-        }
-      })
-      .catch((error) => {
-        console.error('토큰 저장 실패:', error);
-        console.log(idToken);
-      });
-  };
+  //       // 회원 여부에 따른 페이지 이동
+  //       if (response.data.data.isRegistered) {
+  //         nav('/');
+  //       } else {
+  //         nav('/signup', { state: { idToken } });
+  //       }
+  //     })
+  //     .catch((error) => {
+  //       console.error('토큰 저장 실패:', error);
+  //       console.log(idToken);
+  //     });
+  // };
 
-  const handleLoginError = () => {
-    console.log('로그인 실패');
+  // const handleLoginError = () => {
+  //   console.log('로그인 실패');
+  // };
+
+  const googleLogin = async () => {
+    const res = await SocialLogin.login({
+      provider: 'google',
+      options: {
+        scopes: ['email', 'profile'],
+      },
+    });
+    console.log('****login res****');
+    console.log('****login res****');
+    console.log('AccessToken', res.result.accessToken?.token);
+    console.log('****login res****');
+    console.log('****login res****');
   };
 
   return (
@@ -89,10 +104,11 @@ const Login = () => {
           </Typography>
         </div>
         <div css={googleCss}>
-          <GoogleLogin
+          {/* <GoogleLogin
             onSuccess={handleLoginSuccess}
             onError={handleLoginError}
-          />
+          /> */}
+          <button onClick={googleLogin}>Google Login Btn</button>
         </div>
       </div>
     </div>
