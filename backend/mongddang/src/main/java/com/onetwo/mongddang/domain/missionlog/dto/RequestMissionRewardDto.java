aa5aa07fd33a6936496f2db5ef1d0567fe95783b
youngentry <gentry_@naver.com>
2024-11-15 12:58:53 +0900

    ✨ Feat/(#141, S11P31D207-344): 미션 보상 수령 데이터 전송 방식 수정 params -> body

---
+++ backend/mongddang/src/main/java/com/onetwo/mongddang/domain/missionlog/dto/RequestMissionRewardDto.java
@@ -0,0 +1,16 @@
+package com.onetwo.mongddang.domain.missionlog.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RequestMissionRewardDto {
+
+    private Long missionId;
+
+}
