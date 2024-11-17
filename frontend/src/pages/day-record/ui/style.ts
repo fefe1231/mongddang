de1e3044580e99eaa5a77cd408e3695020123fa3
vaaast-lake <kown9512@gmail.com>
2024-11-18 05:18:40 +0900

    🐛 Fix/(#135, S11P31D207-359): 차트 크기 최대 60%, 단 flex grow 1

--- frontend/src/pages/day-record/ui/style.ts
+++ frontend/src/pages/day-record/ui/style.ts
@@ -2,12 +2,15 @@ import { css } from '@emotion/react';
 
 export const article = css`
   background-color: #e1f5fe;
-  height: 100vh;
+  height: calc(100vh - 3rem);
+  display: flex;
+  flex-direction: column;
 `;
 
 export const chart = css`
   /* background-color: white; */
-  height: 55%;
+  flex-grow: 1;
+  max-height: 60%;
   display: flex;
   justify-content: center;
   align-items: center;
