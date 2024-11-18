cb5f2b3a7ca1bd9f72659dacd85d433d01115009
youngentry <gentry_@naver.com>
2024-11-18 17:43:54 +0900

    ✨Feat/(#151, S11P31D207-376): 식사 시작 api 구현

--- backend/mongddang/src/main/java/com/onetwo/mongddang/domain/record/dto/meal/RequestMealStartDto.java
+++ backend/mongddang/src/main/java/com/onetwo/mongddang/domain/record/dto/meal/RequestMealStartDto.java
@@ -2,23 +2,20 @@ package com.onetwo.mongddang.domain.record.dto.meal;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
 
 
 @Getter
-@Setter
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class RequestMealStartDto {
 
     @NotNull(message = "내용은 필수입니다.")
     private String content;
 
-    private MultipartFile imageFile;
+    private String image;
 
     @NotNull(message = "식사 시간은 필수입니다.")
     @Pattern(regexp = "^(breakfast|lunch|dinner|snack)$", message = "식사 시간은 'breakfast', 'lunch', 'dinner', 'snack' 중 하나여야 합니다.")
