732419b2213844e790b8f16ad772f9d1d4395fcc
youngentry <gentry_@naver.com>
2024-11-19 09:01:15 +0900

    ✨ Feat/(#155, S11P31D207-379): 메인 빨간점 데이터 수정

--- backend/mongddang/src/main/java/com/onetwo/mongddang/domain/game/achievement/application/AchievementUtils.java
+++ backend/mongddang/src/main/java/com/onetwo/mongddang/domain/game/achievement/application/AchievementUtils.java
@@ -39,19 +39,17 @@ public class AchievementUtils {
             // 업적에 해당하는 칭호 조회
             Title title = titleRepository.findById(achievement.getId())
                     .orElseThrow(() -> new RestApiException(CustomTitleErrorCode.INVALID_TITLE_ID));
-            log.info("title: {}", title.getName());
+            log.info("title: {}", title.getId());
 
             // 번호에 해당하는 칭호 조회
             Optional<MyTitle> myTitle = myTitleRepository.findByTitleIdAndChildId(title.getId(), childId);
+            log.info("myTitle: {}", myTitle.isEmpty());
 
             // 업적 달성에 요구되는 횟수
             int executionCount = gameLogUtils.getGameLogCountByCategory(childId, achievement.getCategory());
             log.info("executionCount: {}", executionCount);
 
-            // 업적 달성 여부 -1: 달성하지 않음
-            boolean isAchieved = executionCount != -1;
-
-            if (isAchieved && myTitle.isEmpty()) {
+            if (executionCount >= achievement.getCount() && myTitle.isEmpty()) {
                 return true;
             }
         }
