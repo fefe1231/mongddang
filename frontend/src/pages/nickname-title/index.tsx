7672ef621baa37d0314f7a9bebf82c8a8b88c1e5
vaaast-lake <kown9512@gmail.com>
2024-11-21 00:05:34 +0900

    🐛 Fix/(#163, S11P31D207-240): 불필요한 localstorage 저장 삭제 및 상태관리만 유지

--- frontend/src/pages/nickname-title/index.tsx
+++ frontend/src/pages/nickname-title/index.tsx
@@ -9,7 +9,7 @@ import { TitleComponent } from './ui/title-component';
 import { toggleContainerCss } from './ui/styles';
 import { useQuery } from '@tanstack/react-query';
 import { getTitleInfo } from './api/api';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { ItitleData } from './model/types';
 import { useNavigate } from 'react-router-dom';
 
@@ -17,19 +17,8 @@ export const NicknameTitle = () => {
   const [isOn, setIsOn] = useState(false);
   const nav = useNavigate();
 
-  useEffect(() => {
-    // TODO: localStorage -> Preferences로 저장소 수정
-    const savedToggleState = localStorage.getItem('titleToggle');
-    if (savedToggleState) {
-      setIsOn(JSON.parse(savedToggleState));
-    }
-  }, []);
-
   const handleToggle = () => {
-    const newToggleState = !isOn;
-    setIsOn(newToggleState);
-    // TODO: localStorage -> Preferences로 저장소 수정
-    localStorage.setItem('titleToggle', JSON.stringify(newToggleState));
+    setIsOn(!isOn);
   };
 
   const TitleQuery = useQuery({
