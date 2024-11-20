84b61368b137a4f1cd3dc1fcb8de21ceda0b3c60
vaaast-lake <kown9512@gmail.com>
2024-11-21 03:49:18 +0900

    🐛 Fix/(#163, S11P31D207-240): 쿼리 요청 한 번으로 수정

--- frontend/src/shared/lib/queryClient.ts
+++ frontend/src/shared/lib/queryClient.ts
@@ -3,7 +3,7 @@ import { QueryClient } from '@tanstack/react-query';
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      retry: 2,
+      retry: 1,
       staleTime: 0.3 * 60 * 1000,
       gcTime: 0.5 * 60 * 1000,
       refetchOnWindowFocus: false, 
