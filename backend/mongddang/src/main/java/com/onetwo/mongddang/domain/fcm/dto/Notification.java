1efce47bfbc96e6aea2784033689fa808e64c845
변서원 <qustjdnjs@naver.com>
2024-11-20 21:37:26 +0900

    :sparkles: Feat/(#168, S11P31D207-400): 복약 알림에 복약 id값 넣어서 보내주기

--- backend/mongddang/src/main/java/com/onetwo/mongddang/domain/fcm/dto/Notification.java
+++ backend/mongddang/src/main/java/com/onetwo/mongddang/domain/fcm/dto/Notification.java
@@ -1,5 +1,6 @@
 package com.onetwo.mongddang.domain.fcm.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.onetwo.mongddang.domain.user.model.User;
 import lombok.*;
 
@@ -7,11 +8,13 @@ import lombok.*;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Notification {
 
     private User receiver;   // 수신자의 ID 또는 알림 대상
     private User child; // 알림 대상자의 닉네임(이상혈당 지속시)
     private String message;    // 알림 내용
     private String title;      // 알림 제목
+    private Long medicationId;  // 복약 알림 전송 시 전달할 약 id값
 
 }
--- backend/mongddang/src/main/java/com/onetwo/mongddang/domain/fcm/service/notification/Medication.java
+++ backend/mongddang/src/main/java/com/onetwo/mongddang/domain/fcm/service/notification/Medication.java
@@ -3,7 +3,9 @@ package com.onetwo.mongddang.domain.fcm.service.notification;
 import com.onetwo.mongddang.domain.fcm.dto.Notification;
 import com.onetwo.mongddang.domain.fcm.model.PushLog;
 import com.onetwo.mongddang.domain.fcm.service.PushNotificationService;
+import com.onetwo.mongddang.domain.medication.model.MedicationManagement;
 import com.onetwo.mongddang.domain.medication.model.MedicationTime;
+import com.onetwo.mongddang.domain.medication.repository.MedicationManagementRepository;
 import com.onetwo.mongddang.domain.medication.repository.MedicationTimeRepository;
 import com.onetwo.mongddang.domain.record.repository.RecordRepository;
 import com.onetwo.mongddang.domain.user.model.User;
@@ -22,6 +24,7 @@ public class Medication {
     private final RecordRepository recordRepository;
     private final PushNotificationService pushNotificationService;
     private final MedicationTimeRepository medicationTimeRepository;
+    private final MedicationManagementRepository medicationManagementRepository;
 
     @Scheduled(cron = "0 * * * * *") // 매 분마다 실행
     public void sendMedicationReminders() {
@@ -31,12 +34,14 @@ public class Medication {
 
         for (MedicationTime medicationTime : medicationTimes) {
             User child = medicationTime.getMedicationManagement().getChild();
+            Long medicationId = medicationTime.getMedicationManagement().getId();
             String message = "인슐린을 맞을 시간이에요.";
             Notification notification = Notification.builder()
                     .title("복약 알림")
                     .message(message)
                     .receiver(child)
                     .child(child)
+                    .medicationId(medicationId)
                     .build();
             pushNotificationService.sendPushNotification(child, notification, PushLog.Category.medication);
         }
