a1345f58159193ade4e7191ab66716b650e9f6bd
변서원 <qustjdnjs@naver.com>
2024-11-23 17:34:42 +0900

    :sparkles: Feat/(#183, S11P31D207-412): 급식표 조회 기능 구현

--- backend/mongddang/src/main/java/com/onetwo/mongddang/domain/mealplan/error/CustomMealplanErrorCode.java
+++ backend/mongddang/src/main/java/com/onetwo/mongddang/domain/mealplan/error/CustomMealplanErrorCode.java
@@ -11,7 +11,8 @@ public enum CustomMealplanErrorCode implements ErrorCode {
     SCHOOL_INFO_RETR_FAIL(HttpStatus.BAD_GATEWAY, "MP000", "학교 정보 조회에 실패했습니다.\n" +
             "(학교명이 올바른지 확인해주세요.)"),
     MEAL_INFO_RETR_FAIL(HttpStatus.BAD_GATEWAY, "MP001", "급식 정보 조회에 실패했습니다."),
-
+    MEAL_INFO_NOT_FOUND(HttpStatus.NOT_FOUND, "MP002", "급식 정보를 찾을 수 없습니다."),
+    MEAL_TIME_NOT_IN_ENUM(HttpStatus.BAD_REQUEST, "MP003", "mealTime은 breakfast, lunch, dinner 중 하나입니다."),
     ;
 
     private final HttpStatus httpStatus;
--- backend/mongddang/src/main/java/com/onetwo/mongddang/domain/mealplan/repository/MealplanRepository.java
+++ backend/mongddang/src/main/java/com/onetwo/mongddang/domain/mealplan/repository/MealplanRepository.java
@@ -3,6 +3,24 @@ package com.onetwo.mongddang.domain.mealplan.repository;
 import com.onetwo.mongddang.domain.mealplan.model.Mealplan;
 import com.onetwo.mongddang.domain.user.model.CtoP;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 public interface MealplanRepository extends JpaRepository<Mealplan, Long> {
+
+    @Query(value = "SELECT * FROM mealplan m " +
+            "WHERE m.child_id = :childId " +
+            "AND m.meal_time = :mealTime " +
+            "AND m.start_time >= CURRENT_DATE " +
+            "AND m.start_time < CURRENT_DATE + INTERVAL 1 DAY " +
+            "LIMIT 1",
+            nativeQuery = true)
+    Optional<Mealplan> findOneByChildIdAndMealTimeToday(
+            @Param("childId") Long childId,
+            @Param("mealTime") String mealTime);
+
 }
